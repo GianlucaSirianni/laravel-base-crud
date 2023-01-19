762321989b500910a6b528408a39053d74439d1f
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-01-19 14:27:11 +0100

    parte 1

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_comic = config('fumetti');
+
+        foreach ($array_comic as $elem) {
+            $array_comic = new Comic();
+            $array_comic->title = $elem['title'];
+            $array_comic->description = $elem['description'];
+            $array_comic->thumb = $elem['thumb'];
+            $array_comic->price = $elem['price'];
+            $array_comic->series = $elem['series'];
+            $array_comic->sale_date = $elem['sale_date'];
+            $array_comic->type = $elem['type'];
+            $array_comic->save();
+        };
+    }
+}
