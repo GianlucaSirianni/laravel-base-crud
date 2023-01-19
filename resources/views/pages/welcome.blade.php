7bc8b1e099658782e9a35adf14e20e518fc68a3f
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-01-19 17:34:30 +0100

    manca css

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('title-page', 'DC COMICS - HOME')
 
-@section('main-content')
-    <h1>Home Page</h1>
+@section('main-content')    
+    <div class="homeP">
+        
+        <h1>Home Page</h1>
+    </div>
+    
 @endsection
